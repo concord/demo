a3abbbf8522c6a0aba90bd74c74b1da9dbf923bd
agallego <gallego.alexx@gmail.com>
2016-04-01 12:15:58 -0400

    Keeping metrics of unique and non-unique thingss

--- liberty/src/unique/unique.cc
+++ liberty/src/unique/unique.cc
@@ -38,13 +38,18 @@ class Unique final : public bolt::Computation {
   }
 
   virtual void processRecord(CtxPtr ctx, bolt::FrameworkRecord &&r) override {
-    bloom_add(&bloom_, (void *)r.value.c_str(), r.value.length());
+    ++recordCount_;
+    if(bloom_check(&bloom_, (void *)r.value.c_str(), r.value.length()) == 0) {
+      ++uniqueRecords_;
+      bloom_add(&bloom_, (void *)r.value.c_str(), r.value.length());
+    }
   }
 
   virtual void
   processTimer(CtxPtr ctx, const std::string &key, int64_t time) override {
     ctx->setTimer(key, bolt::timeNowMilli() + 10000);
-    LOG(INFO) << "bloom_: " << bloom_;
+    LOG(INFO) << "unique records: " << uniqueRecords_
+              << ", total records: " << recordCount_ << ", bloom_: " << bloom_;
   }
 
   virtual bolt::Metadata metadata() override {
@@ -57,6 +62,8 @@ class Unique final : public bolt::Computation {
 
   private:
   struct bloom bloom_;
+  uint64_t recordCount_{0};
+  uint64_t uniqueRecords_{0};
 };
 
 int main(int argc, char *argv[]) {
