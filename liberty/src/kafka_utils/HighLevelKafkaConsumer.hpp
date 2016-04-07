b2078a6c90ab2522ae88ca0ebbe8b4e423c0f891
agallego <gallego.alexx@gmail.com>
2016-04-07 16:55:16 -0400

    Testing round-robing consumer

--- liberty/src/kafka_utils/HighLevelKafkaConsumer.hpp
+++ liberty/src/kafka_utils/HighLevelKafkaConsumer.hpp
@@ -103,6 +103,7 @@ class HighLevelKafkaConsumer : public RdKafka::EventCb,
       {"receive.message.max.bytes", "100000000"}, // Max receive buff or 100MB
       {"fetch.message.max.bytes", "20000"},       // Some smmalller default
       {"statistics.interval.ms", "60000"},        // every minute
+      {"partition.assignment.strategy", "roundrobin"} // better for consuming
     };
     if(FLAGS_enable_kafka_consumer_debug) {
       defaultOpts.insert({"debug",
