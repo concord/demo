799c8242d687c6294402632f2e25433e6db40d56
a <asher@yieldmo.com>
2016-04-07 00:12:17 -0400

    Deduplication job almost ready for testing

---
+++ liberty/spark/build.sbt
@@ -0,0 +1,19 @@
+name := "spark"
+organization:= "concordsystems"
+version := "0.1"
+scalaVersion := "2.11.8"
+
+resolvers ++= Seq(
+  "Clojars" at "https://clojars.org/repo/",
+  "Conjars" at "http://conjars.org/repo/",
+  "Maven-Repository.com" at "http://repo1.maven.org/maven2/",
+  "Central" at "http://central.maven.org/maven2/",
+  Resolver.sonatypeRepo("public")
+)
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-core_2.11" % "1.6.1",
+  "org.apache.spark" % "spark-streaming_2.11" % "1.6.1",
+  "org.apache.spark" % "spark-streaming-kafka_2.11" % "1.6.1",
+  "args4j" % "args4j" % "2.33"
+)
---
+++ liberty/spark/src/main/scala/SparkArgHelper.scala
@@ -0,0 +1,43 @@
+package concord.demo.liberty.spark
+
+import org.kohsuke.args4j.{CmdLineException, CmdLineParser, Option}
+import scala.collection.JavaConversions._
+
+/**
+  * Created by adev on 4/4/16.
+  */
+class SparkArgHelper(args: Array[String]) {
+
+  object CliArgs {
+    @Option(name = "-master", usage = "Spark master url")
+    var masterUrl: String = "local[*]"
+
+    @Option(name = "-input", usage = "kafka or text")
+    var input: String = "text"
+
+    @Option(name = "-kafka.metadata.broker.list", usage = "kafka broker info")
+    var kafka_metadata_broker_list: String = ""
+
+    @Option(name = "-kafka.topics", usage = "kafka topics separated by ,")
+    var kafka_topic_list: String = ""
+
+    @Option(name = "inputFile", usage = " for text file input")
+    var inputFile: String = "/tmp/sample.txt"
+
+    @Option(name = "output", usage = "kafka, cassandra, or text")
+    var output: String = "text"
+
+    @Option(name = "outputDetails", usage = "kafka info, cassandra info, or text file location")
+    var outputDetails: String = "/tmp/sample.txt"
+  }
+
+  val parser = new CmdLineParser(CliArgs)
+  try {
+    parser.parseArgument(args.toList)
+  } catch {
+    case e: CmdLineException =>
+      print(s"Error:${e.getMessage}\n Usage:\n")
+      parser.printUsage(System.out)
+      System.exit(1)
+  }
+}
