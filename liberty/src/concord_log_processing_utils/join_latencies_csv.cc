9aeabf1e7b6afbd84b3d212231b88020e3553db8
agallego <gallego.alexx@gmail.com>
2016-04-11 12:02:07 -0400

    Join ALL csv file types

--- liberty/src/concord_log_processing_utils/join_latencies_csv.cc
+++ liberty/src/concord_log_processing_utils/join_latencies_csv.cc
@@ -6,90 +6,134 @@
 #include <ios>
 
 DEFINE_string(latency_files, "", "coma delimited list of files");
+DEFINE_string(hardware_files, "", "coma delimited list of files");
 DEFINE_string(output_csv, "latency_aggregate.csv", "csv output file");
 
 
-class LatencyFileIterator {
+class FileIterator {
   public:
-  LatencyFileIterator(std::string filename) : filename_(filename) {
+  virtual ~FileIterator() {}
+  FileIterator(std::string filename) : filename_(filename) {
     ifl_.open(filename_, std::ios::in);
     if(ifl_.fail()) {
       ifl_.clear();
       LOG(FATAL) << "File " << filename_ << ", failed to open";
     }
   }
+  virtual const std::string &delimiter() const = 0;
+  virtual const std::string header() const = 0;
   std::string nextLine() {
-    static const std::string kDelimiter = ",,,";
     ++lines_;
     std::string ret = ""; // empty csv
     if(!ifl_.eof()) {
       std::getline(ifl_, ret);
-    } else {
-      LOG(INFO) << "Got eof for file: " << filename_
-                << " produced lines: " << lines_;
     }
     if(ret.empty()) {
-      return kDelimiter;
+      return delimiter();
     }
     return ret;
   }
 
-  private:
+  protected:
   uint32_t lines_{0};
   std::string filename_;
   std::ifstream ifl_;
 };
 
 
+class LatencyFileIterator : public FileIterator {
+  public:
+  LatencyFileIterator(std::string filename) : FileIterator(filename) {}
+  virtual const std::string &delimiter() const override {
+    static const std::string kDelimiter = ",,,";
+    return kDelimiter;
+  }
+  virtual const std::string header() const override {
+    auto f = filename_;
+    for(auto j = 0u; j < f.size(); ++j) {
+      if(f[j] == ':' || f[j] == '/' || f[j] == '.' || f[j] == '-') {
+        f[j] = '_';
+      }
+    }
+    return f + "_date," + f + "_time," + f + "_p50," + f + "_p999";
+  }
+};
+
+class HardwareFileIterator : public FileIterator {
+  public:
+  HardwareFileIterator(std::string filename) : FileIterator(filename) {}
+  virtual const std::string &delimiter() const override {
+    static const std::string kDelimiter = ",,,,,,";
+    return kDelimiter;
+  }
+  virtual const std::string header() const override {
+    auto f = filename_;
+    for(auto j = 0u; j < f.size(); ++j) {
+      if(f[j] == ':' || f[j] == '/' || f[j] == '.' || f[j] == '-') {
+        f[j] = '_';
+      }
+    }
+    return f + "_date," + f + "_time," + f + "_1min," + f + "_5min," + f
+           + "_free_mem_gb," + f + "_used_mem_gb," + f + "_percentage_used";
+  }
+};
+
+
+std::vector<std::string> split_files(const std::string &line) {
+  std::vector<std::string> out{};
+  folly::split(",", line, out, true);
+  return out;
+}
+
 int main(int argc, char *argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
-  CHECK(!FLAGS_latency_files.empty()) << "cannot be empty";
-  std::vector<std::string> files{};
-  folly::split(",", FLAGS_latency_files, files);
-  std::vector<LatencyFileIterator> itrs{};
-  for(auto &f : files) {
-    if(!f.empty()) {
-      itrs.emplace_back(f);
-    }
+  CHECK(!FLAGS_latency_files.empty() || !FLAGS_hardware_files.empty())
+    << "cannot be empty";
+
+  std::vector<std::string> latencyFileNames = split_files(FLAGS_latency_files);
+  std::vector<std::string> hardwareFileNames =
+    split_files(FLAGS_hardware_files);
+
+  std::vector<std::unique_ptr<FileIterator>> itrs{};
+
+  for(auto &f : latencyFileNames) {
+    auto it = std::make_unique<LatencyFileIterator>(f);
+    itrs.push_back(std::move(it));
+  }
+  for(auto &f : hardwareFileNames) {
+    auto it = std::make_unique<HardwareFileIterator>(f);
+    itrs.push_back(std::move(it));
   }
   std::ofstream ofl(FLAGS_output_csv);
   if(ofl.fail()) {
     ofl.clear();
     LOG(FATAL) << "could not open " << FLAGS_output_csv;
   }
-  for(auto i = 0u; i < files.size(); ++i) {
-    auto f = files[i];
-    if(f.size() > 25) {
-      f = f.substr(std::min(f.size(), f.size() - 25), 21);
-    }
-    // replace / and : and other chars
-    for(auto j = 0u; j < f.size(); ++j) {
-      if(f[j] == ':' || f[j] == '/' || f[j] == '.' || f[j] == '-') {
-        f[j] = '_';
-      }
-    }
+  for(auto i = 0u; i < itrs.size(); ++i) {
+    auto &it = itrs[i];
     if(i != 0) {
       ofl << ",";
     }
-    ofl << f + "_date," << f + "_time," << f + "_p50," << f + "_p999";
+    ofl << it->header();
   }
   // add new line after header
   ofl << std::endl;
   // skip the header
-  for(auto &i : itrs) {
-    i.nextLine();
+  for(const auto &i : itrs) {
+    i->nextLine();
   }
 
   auto lines = itrs.size();
   while(true) {
     for(auto i = 0u; i < itrs.size(); ++i) {
-      auto tmp = std::move(itrs[i].nextLine());
+      auto &it = itrs[i];
+      auto tmp = std::move(it->nextLine());
       if(i != 0) {
         ofl << ",";
       }
-      if(tmp == ",,,") {
+      if(tmp == it->delimiter()) {
         lines -= 1;
       }
       ofl << tmp;
