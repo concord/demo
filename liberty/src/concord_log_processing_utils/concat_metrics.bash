236c115c430c360d079e658295d189cb4cf54dd2
agallego <gallego.alexx@gmail.com>
2016-04-11 10:18:31 -0400

    Working bash script

--- liberty/src/concord_log_processing_utils/concat_metrics.bash
+++ liberty/src/concord_log_processing_utils/concat_metrics.bash
@@ -1,22 +1,11 @@
 #!/bin/bash --login
 set -e
-# tar_file="$1"
-# work_dir=$(mktemp -d)
-# cd $work_dir
-# gsutil -m cp "gs://ephemeral-public/$tar_file" "$work_dir"
-# tar -xzf "$work_dir/$tar_file" -C "$work_dir"
-
-
-work_dir="/tmp/tmp.1v5m77lvWS"
+git_root=$(git rev-parse --show-toplevel)
+tar_file="$1"
+work_dir=$(mktemp -d)
 cd $work_dir
-
-
-for f in $(ls "$work_dir"); do
-    f="$work_dir/$f"
-    if [[ -d "$f" ]]; then
-        for dir in $(ls "$f"); do
-            dir="$f/$dir"
-            find "$dir" -iname "latency.csv" -print
-        done
-    fi
-done
+gsutil -m cp "gs://ephemeral-public/$tar_file" "$work_dir"
+tar -xzf "$work_dir/$tar_file" -C "$work_dir"
+files=$(find "$work_dir" -iname "latency.csv" -print)
+files=$(echo "$files" | tr '\n' ',')
+$git_root/build/join_latency_csv --latency_files="$files" --output_csv="$work_dir/latency_aggregate.csv"
