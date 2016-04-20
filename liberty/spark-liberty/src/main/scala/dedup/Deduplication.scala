aead616094351ac96077decad929d2e96f0a07be
agallego <gallego.alexx@gmail.com>
2016-04-20 13:04:26 -0400

    Unique doesn't compile in master

--- liberty/spark-liberty/src/main/scala/dedup/Deduplication.scala
+++ liberty/spark-liberty/src/main/scala/dedup/Deduplication.scala
@@ -21,14 +21,17 @@ class Deduplication(
   }
 
   def streamPayloadOnly: Unit = {
-    val data = stream map { line =>
-      val wholeLine = LogParser.parse(line._2)
-      (wholeLine.get.msg, wholeLine) }
-    val uniques = data.reduceByKey(_ _)
-    val tsUpFront = uniques map { line => (line._1.get.timestamp, line._1.get.buildValue) }
-    // //Maybe not needed depending on equals & hashcode of LogLine?
-    val sorted = tsUpFront.transform(rdd => rdd.sortByKey(true))
-    sorted.print()
+    val data = stream.flatMap { line =>
+      LogParser.parse(line._2)
+    }.map { logparser =>
+      (logparser.msg, logparser)
+    }
+    // TODO(asher) - this doesn't compile
+    // val uniques = data.reduceByKey((m: String, l: LogParser) => m)
+    // val tsUpFront = uniques map { line => (line._1.get.timestamp, line._1.get.buildValue) }
+    // // //Maybe not needed depending on equals & hashcode of LogLine?
+    // val sorted = tsUpFront.transform(rdd => rdd.sortByKey(true))
+    // sorted.print()
   }
 
   override def streamLogic: Unit = {
