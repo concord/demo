5c524cdae0b38d8b9f77c94af53a51835aa91834
agallego <gallego.alexx@gmail.com>
2016-04-21 15:44:08 -0400

    Fixing approx to work w/ kafka - bad context

--- liberty/spark-liberty/src/main/scala/dedup/ApproxDeduplication.scala
+++ liberty/spark-liberty/src/main/scala/dedup/ApproxDeduplication.scala
@@ -1,94 +1,99 @@
 package com.concord.dedup
-
-import com.concord.contexts.BenchmarkStreamContext
 import com.concord.utils.LogParser
-import org.apache.spark.streaming.{Duration, Seconds}
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.{ Duration, Seconds }
 import org.apache.spark.broadcast.Broadcast
 /**
-  *  Algebird is needed w/ wild card for, serialization,
-  *  bloom filters and imlicit conversions.
-  */
+ *  Algebird is needed w/ wild card for, serialization,
+ *  bloom filters and imlicit conversions.
+ */
 import com.twitter.algebird._
-import org.kohsuke.args4j.{CmdLineException, CmdLineParser, Option}
+import org.kohsuke.args4j.{ CmdLineException, CmdLineParser, Option => CLIOption }
 import org.apache.kafka.clients.producer.{
   KafkaProducer,
   ProducerRecord
 }
-
-
-class KafkaSink(createProducer: () => KafkaProducer[String, String])
-    extends Serializable {
-  lazy val producer = createProducer()
-  def send(topic: String, key: String, value: String): Unit =
-    producer.send(new ProducerRecord(topic, key, value))
-}
+import kafka.serializer.StringDecoder
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.dstream.DStream
 
 object KafkaSink {
-  def apply(brokers: String): KafkaSink = {
-    val f = () => {
-      val props = new java.util.Properties()
-      props.put("bootstrap.servers", brokers)
-      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      println("Creating new kafka producer")
-      val producer = new KafkaProducer[String, String](props)
-      sys.addShutdownHook {
-        producer.close()
-      }
-      producer
-    }
-    new KafkaSink(f)
+  def newProducer(brokers: String): KafkaProducer[String, String] = {
+    val props = new java.util.Properties()
+    props.put("bootstrap.servers", brokers)
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    new KafkaProducer[String, String](props)
   }
 }
 
 class ApproxDeduplication(
-  override val brokers: String,
-  override val topics: Set[String],
-  val outputTopic: String)
-    extends BenchmarkStreamContext {
-  override def batchInterval: Duration = Seconds(1)
-  override def streamingRate: Int = 15000
-  override def applicationName: String = "ApproxDeduplication"
+    val brokers: String,
+    val topics: Set[String],
+    val outputTopic: String
+) extends Serializable {
+  def defaultSparkConf: SparkConf = {
+    val conf = new SparkConf()
+      .setAppName(applicationName)
+      .set("spark.mesos.executor.home", "/usr/lib/spark")
+      .set("spark.default.parallelism", "120")
+      .set("spark.streaming.kafka.maxRatePerPartition", streamingRate.toString)
+    conf
+  }
+  private val kafkaParams = Map[String, String](
+    "metadata.broker.list" -> brokers,
+    "auto.offset.reset" -> "smallest"
+  )
 
-  override def streamLogic: Unit = {
+  def batchInterval: Duration = Seconds(1)
+  def streamingRate: Int = 8000
+  def applicationName: String = "ApproxDeduplication"
+  def start(): Unit = {
+    val sparkContext = new SparkContext(defaultSparkConf)
+    val streamingSparkContext = new StreamingContext(sparkContext, batchInterval)
+    val stream = KafkaUtils
+      .createDirectStream[String, String, StringDecoder, StringDecoder](
+        streamingSparkContext, kafkaParams, topics
+      )
     val kProbFalsePositive = 0.08
     val kPopulation = 265569231
     val kSeed = 1
     val bfMonoid = BloomFilter(kPopulation, kProbFalsePositive, kSeed)
-
     var bf = bfMonoid.zero
-
-    val kafkaOut = stream.flatMap { line =>
+    stream.flatMap { line =>
       LogParser.parse(line._2) match {
         case Some(x) =>
-          if(!bf.contains(x.msg).isTrue){
+          if (!bf.contains(x.msg).isTrue) {
             bf = bf ++ bfMonoid.create(x.msg)
             Some((x.buildKey.toString, x.buildValue))
-          }else {
+          } else {
             None
           }
         case None => None
       }
-    }
-    val broadcast: Broadcast[KafkaSink] =
-      kafkaOut.context.sparkContext.broadcast(KafkaSink(brokers))
-    kafkaOut.foreachRDD{ rdd => (
-      rdd.foreachPartition(partition => {
-        partition.foreach { case (k,v) =>
-          broadcast.value.send(outputTopic,k,v)
-        }
+    }.foreachRDD(rdd => {
+      rdd.foreachPartition(part => {
+        val producer = KafkaSink.newProducer(brokers)
+        part.foreach(record =>
+          producer.send(new ProducerRecord(outputTopic, record._1, record._2)))
+        producer.close
       })
-    )}
+    })
+    streamingSparkContext.start()
+    streamingSparkContext.awaitTermination()
   }
 }
 
 class ApproxArgs(args: Array[String]) {
   object cli {
-    @Option(name = "-kafka_brokers", usage = "i.e. localhost:9092,1.1.1.2:9092")
+    @CLIOption(name = "-kafka_brokers", usage = "i.e. localhost:9092,1.1.1.2:9092")
     var kafkaBrokers: String = ""
-    @Option(name = "-output_topic", usage = "kafka topics separated by ,")
+    @CLIOption(name = "-output_topic", usage = "kafka topics separated by ,")
     var outputTopic: String = "approx_uniq"
-    @Option(name = "-input_topic", usage = "kafka topics separated by ,")
+    @CLIOption(name = "-input_topic", usage = "kafka topics separated by ,")
     var kafkaTopics: String = "liberty"
   }
   val parser = new CmdLineParser(cli)
